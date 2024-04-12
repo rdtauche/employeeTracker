96ac78ab4a44ed0e1adc79b8e9a6ab8b564475a7
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-04-12 07:30:25 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+\c employeeTracker_db;
+
+-- Creates the table "departments" within inventory_db --
+CREATE TABLE departments (
+  -- Creates a numeric column called "departmentid" --
+  departmentID INTEGER,
+  -- Creates a string column called "departmentname" which can hold up to 100 characters --
+  departmentName VARCHAR(100)
+);
+
+CREATE TABLE roles (
+  roleID INTEGER,
+  jobTitle VARCHAR(100),
+  salary INTEGER,
+  departmentID INTEGER
+);
+
+CREATE TABLE employees (
+  employeeID INTEGER,
+  firstName VARCHAR(100),
+  lastName VARCHAR(100),
+  jobTitle VARCHAR(100),
+  manager VARCHAR(100),
+  salary INTEGER,
+  departmentID INTEGER
+);
+
