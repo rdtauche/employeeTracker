31c7ed40f366a686d29fe428d573e06e43894ab2
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-04-13 07:29:20 -0400

    added server.js file and established connectivity to postgres db

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+// Import and require Pool (node-postgres)
+// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
+const { Pool } = require("pg");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const pool = new Pool(
+  {
+    // Enter PostgreSQL username
+    user: "postgres",
+    // Enter PostgreSQL password
+    password: "Xadrez1982!!",
+    host: "localhost",
+    database: "employeetracker_db",
+  },
+  console.log("Connected to the employeetracker_db database!")
+);
+
+pool.connect();
+
+// Hardcoded query: DELETE FROM course_names WHERE id = 3;
+pool.query(`DELETE FROM course_names WHERE id = $1`, [3], (err, { rows }) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(rows);
+});
+
+// Query database
+pool.query(
+  "SELECT * FROM course_names where id=$1",
+  [2],
+  function (err, { rows }) {
+    console.log(rows);
+  }
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
